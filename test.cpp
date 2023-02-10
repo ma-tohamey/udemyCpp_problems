c99ea16492e2367049a87bb178a68fcd0f267e23
ma-tohamey <mahmoud.altohamey@aol.com>
2023-02-10 11:48:13 +0200

    adding test cpp app

---
+++ test.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+  string a {"practice"};
+  string b {"perfect"};
+  cout << (a < b);
+  return 0;
+  // int a = 0, b = 1;
+
+  // cout << a++ << endl;
+  // cout << ++a << endl;
+  // a = ++b;
+  // cout << a << " " << b << endl;
+  // ++b = a;
+  // cout << a << " " << b << endl;
+  // a = b++;
+  // b++ = a;
+
+  // cout << a << " " << b << endl;
+  // return 0;
+
+
+}
+
+// int main()
+// {
+//   int a, b;
+//
+//   cout << "Please enter 2 number?\n";
+//
+//   cin >> a >> b;
+//
+//   cout << a * b << " " << a + b << "\n";
+//
+//   return 0;
+// }
+// int main()
+// {
+//   int a = 10;
+//   int b = 21;
+//
+//   int i1 = a + b / 2;
+//   int i2 = (a + b) / 2;
+//   cout << i1 << " " << i2 << endl;
+//
+//   double x = 10.0;
+//   double y = 21;
+//
+//   double d1 = x + y / 2.0;
+//   double d2 = (x + y) / 2.0;
+// }
